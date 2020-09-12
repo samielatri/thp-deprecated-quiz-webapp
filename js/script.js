52ac6429f1346df9bc88acac180370bc74377ef7
samielatri <sami.elatri@yahoo.com>
2020-09-13 01:11:50 +0200

    hotfix bug loading googlsheet and add console log to verify selection

--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@
 
 // information area : variable to hold the output information
 const informationArea = document.querySelector('.information') ;
+console.log(informationArea) ;
 // question area : variable to hold the output question
 const questionArea = document.querySelector('.question') ;
+console.log(questionArea) ;
 // next button ! varaible to hold the next button
 const nextButton = document.querySelector('.next-button') ;
-
+console.log(nextButton) ;
 /* questionURL object */
 
 // question url to connect to
-const questionURL = 'https://script.google.com/macros/s/AKfycbxRgyvagg_MhCRSqDZnnzNDybLO9u92-Q-RGtG_Yp-Gx8MSMGcJ/exec' ;
+const questionUrl = 'https://script.google.com/macros/s/AKfycbxRgyvagg_MhCRSqDZnnzNDybLO9u92-Q-RGtG_Yp-Gx8MSMGcJ/exec' ;
 
 /* global object */
 
@@ -28,18 +30,18 @@ fetch(questionUrl).then(function(questionResult) {
   // return questionUrl in json format
   return questionResult.json() ;
 }).then(function(question){
-  // print the questionUrl in json format in console to verify that it is loaded
+  // print the questionResult in json format in console to verify that it is loaded
   console.log(question.data) ;
   //
-  quiz.total = data.data.length ;
+  quiz.total = question.data.length ;
   //
   quiz.val = 0 ;
   //
   quiz.score = 0 ;
   //
-  quiz.arr = data.data ;
+  quiz.arr = question.data ;
   //
-  data.data.forEach(function(el) {
+  question.data.forEach(function(el) {
     console.log(el) ;
   })
   createQuestion() ;
