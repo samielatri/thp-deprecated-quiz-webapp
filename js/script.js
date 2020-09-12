c8a75888becd8f0ec2d087445e9e09ad7cd90a52
samielatri <sami.elatri@yahoo.com>
2020-09-12 23:11:41 +0200

    add constants from slected page elements and a global object to hold all quiz data in js/script.js file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* constants */
+
+/* selected page elements */
+
+// information area : variable to hold the output information
+const informationArea = document.querySelector('.information') ;
+// question area : variable to hold the output question
+const questionArea = document.querySelector('.question') ;
+// next button ! varaible to hold the next button
+const nextButton = document.querySelector('.next-button') ;
+
+/* global object */
+
+// quiz : global object to hold the quiz data
+const quiz = {} ;
