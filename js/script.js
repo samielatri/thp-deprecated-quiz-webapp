f1c005162aee01e316a9968d2968d21acf17b810
samielatri <sami.elatri@yahoo.com>
2020-09-12 23:16:25 +0200

    add eventListener on click event for nextbutton with basic function createQuestion in js/script.js file

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ const nextButton = document.querySelector('.next-button') ;
 
 // quiz : global object to hold the quiz data
 const quiz = {} ;
+
+/* eventListeners */
+
+// onClick, it creates the new question
+nextButton.addEventListener('click', createQuestion) ;
+
+// create question
+function createQuestion() {
+  // the next button disappears
+  nextButton.style.display = "none" ;
+
+}
+
