0943c1de23d43477cdd9aa5acf73a7a7104ccde0
samielatri <sami.elatri@yahoo.com>
2020-09-13 22:43:27 +0200

    format to indicate that it is the week0 quiz

--- js/script.js
+++ js/script.js
@@ -12,10 +12,7 @@ console.log(questionArea) ;
 const nextButton = document.querySelector('.next-button') ;
 console.log(nextButton) ;
 
-/* questionURL object */
 
-// question url to connect to
-const questionUrl = 'https://script.google.com/macros/s/AKfycbxRgyvagg_MhCRSqDZnnzNDybLO9u92-Q-RGtG_Yp-Gx8MSMGcJ/exec' ;
 
 /* global object */
 
