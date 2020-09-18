db744831162c2b82892e67ab5dd0f7987e379b9a
samielatri <sami.elatri@yahoo.com>
2020-09-18 03:20:44 +0200

    add quiz for ruby

--- js/rubyonrails.js
+++ js/rubyonrails.js
@@ -1,4 +1,4 @@
 /* questionURL object */
 
 // question url to connect to
-const questionUrl = 'https://script.google.com/macros/s/AKfycbxRgyvagg_MhCRSqDZnnzNDybLO9u92-Q-RGtG_Yp-Gx8MSMGcJ/exec' ;
+const questionUrl = 'https://script.google.com/macros/s/AKfycbzH49qtaM5r-woa5y7WoERqhK57onnFJ4e1zlrFu2aAr4hx7Lg/exec' ;
